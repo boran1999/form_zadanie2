41d708e0a5ea75f9ee0960e2e4f0872d3188a499
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-04-11 19:18:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <h2 align ="left"><font color="blue">Форма регистрации</font></h2>
+  <title>Test_Form</title>
+ </head>
+ <body bgcolor="lightblue">
+	<?php
+		$filelist = glob("*.txt");
+		$i=0;
+	?>
+	<form action="form2.php" method="POST">
+		<?php
+			foreach ($filelist as $filename){
+				echo "<input type='checkbox' name='f[]' value=".$filename."><font color='blue'>".$filename."</font><br>";
+			}
+		?>
+	<p><input type="submit" value="Отправить данные"></p>
+	</form>
+ </body>
+</html>
