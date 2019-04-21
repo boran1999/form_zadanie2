04f13efe33e09302dac2a666cc911622bdef72ae
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-04-21 22:31:35 +0800

    Update form2.php

--- form2.php
+++ form2.php
@@ -11,13 +11,19 @@
 				echo ("вы ничего не выбрали"); 
 			} 
 			else{
-				echo"<h3>Данные файлы успешно удалены:</h3>";
 				$af=$_POST['f'];
 				$n=count($af);
 				for($i=0;$i<$n;$i++){
-					if(!empty($af[$i])){
-						echo ($af[$i]."<br>");
-						unlink($af[$i]);	
+					if(!empty($af[$i]) && substr($af[$i], -3)=="txt"){
+						unlink("logs/$af[$i]");	
+					}
+				}
+				for($i=0;$i<$n;$i++){
+					if(!file_exists("logs/$af[$i]")){
+						echo "<p><b>".$af[$i]."</b>  файл успешно удален</p>";
+					}
+					else{
+						echo "<p><b>".$af[$i]."</b>  файл не удалось удалить по какой-то причине</p>";
 					}
 				}
 			}
